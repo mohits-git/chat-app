a92ea10f962bbce23978e901b065c52debd1618f
mohits-git <mohitkhatri2805@gmail.com>
2024-05-09 21:50:06 +0530

    init: setup mongo db connection

--- server/index.js
+++ server/index.js
@@ -2,17 +2,21 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
+const connectDB = require('./config/connect-db');
+
 const app = express();
-app.use(cors({ 
-    origin: process.env.FRONTEND_URL,
-    credentials: true
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  credentials: true
 }));
 
 app.get('/', (req, res) => {
-    res.json({
-        message: `Server running at ${PORT}`
-    })
-})
+  res.json({
+    message: `Server running at ${PORT}`
+  })
+});
 
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => console.log(`Server listening at port ${PORT}`));
+connectDB().then(() => {
+  const PORT = process.env.PORT || 8080;
+  app.listen(PORT, () => console.log(`Server listening at port ${PORT}`));
+});
