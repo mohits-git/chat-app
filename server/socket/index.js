c025dfc95caed1e49f788d748b309b361d6873a9
mohits-git <mohitkhatri2805@gmail.com>
2024-05-12 20:55:45 +0530

    feat: added websocket methods handlers for new messages and new connections

--- server/socket/index.js
+++ server/socket/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const { Server } = require('socket.io')
 const http = require('http');
 const getUserDetailsFromToken = require('../helper/get-user-details-from-token');
+const UserModel = require('../models/user-model');
+const ConversationModel = require('../models/conversation-model');
+const MessageModel = require('../models/message-model');
 
 const app = express();
 
@@ -21,10 +24,11 @@ io.on("connection", async (socket) => {
   const token = socket.handshake.auth.token;
 
   const currentUserDetails = await getUserDetailsFromToken(token);
-  if(!currentUserDetails.logout) {
+  if (!currentUserDetails.logout) {
     currentUserDetails;
-    socket.join(currentUserDetails?._id);
-    onlineUsers.add(currentUserDetails?._id)
+    socket.join(currentUserDetails?._id.toString());
+    onlineUsers.add(currentUserDetails?._id?.toString());
+
     io.emit('onlineUser', Array.from(onlineUsers));
   }
 
@@ -32,6 +36,83 @@ io.on("connection", async (socket) => {
     onlineUsers.delete(currentUserDetails?._id);
     console.log(`Disconnected user ${socket.id}`);
   });
+
+  socket.on("message-page", async (userId) => {
+    try {
+      console.log("userId from Message page:", userId)
+      const userDetails = await UserModel.findById(userId).select("-password");
+      if (!userDetails) {
+        socket.emit("message-user", { error: true, message: "User not found" });
+        return;
+      }
+      const payload = {
+        _id: userDetails._id,
+        name: userDetails.name,
+        email: userDetails.email,
+        profile_pic: userDetails.profile_pic,
+        online: onlineUsers.has(userId),
+      }
+
+      socket.emit("message-user", payload);
+
+      const sender = currentUserDetails._id.toString(), receiver = userId.toString();
+      const getConversation = await ConversationModel.findOne({
+        "$or": [
+          { sender, receiver },
+          { sender: receiver, receiver: sender },
+        ]
+      }).populate('messages')
+        .sort({ updatedAt: -1 });
+      if(!getConversation) return;
+      io.in(sender).emit('message', getConversation);
+      io.in(receiver).emit('message', getConversation);
+
+    } catch (error) {
+      console.log(error);
+      socket.emit("message-user", { error: true, message: "User not found" });
+    }
+  });
+
+  socket.on("new-message", async (data) => {
+    try {
+      const { sender, receiver, text, imageUrl, videoUrl } = data;
+      let conversation = await ConversationModel.findOne({
+        "$or": [
+          { sender, receiver },
+          { sender: receiver, receiver: sender },
+        ]
+      });
+      console.log("Conversation:", conversation)
+      if (!conversation) {
+        const newConversation = await ConversationModel.create({
+          sender,
+          receiver,
+        });
+        conversation = newConversation;
+      }
+
+      const newMessage = await MessageModel.create({
+        text,
+        imageUrl,
+        videoUrl,
+        sender,
+      });
+
+      await ConversationModel.updateOne({ _id: conversation._id }, {
+        "$push": { messages: newMessage._id }
+      });
+
+      const getConversation = await ConversationModel
+        .findById(conversation._id)
+        .populate('messages')
+        .sort({ updatedAt: -1 });
+
+      io.in(sender).emit('message', getConversation);
+      io.in(receiver).emit('message', getConversation);
+    } catch (error) {
+      console.log(error);
+    }
+  })
 });
 
 module.exports = {
