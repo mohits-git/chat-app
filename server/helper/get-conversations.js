c6fe566c4fc8765ce9de85fb994cc6579ae3cf8c
mohits-git <mohitkhatri2805@gmail.com>
2024-05-12 23:02:06 +0530

    feat: added get-conversations helper function

---
+++ server/helper/get-conversations.js
@@ -0,0 +1,26 @@
+const ConversationModel = require("../models/conversation-model");
+
+async function getConversations(userId) {
+  if (!userId) return;
+  const userConversation = await ConversationModel.find({
+    "$or": [
+      { sender: userId },
+      { receiver: userId },
+    ]
+  }).populate('messages').populate('sender').populate('receiver').sort({ updatedAt: -1 });
+
+  const conversations = userConversation.map((convo) => {
+    const unseenMessages = convo.messages.reduce((total, curr) => total + (!curr.seen ? 1 : 0), 0);
+    return {
+      _id: convo?._id,
+      sender: convo?.sender,
+      receiver: convo?.receiver,
+      unseen: unseenMessages,
+      lastMessage: convo.messages[convo?.messages?.length - 1] || '',
+    }
+  });
+
+  return conversations;
+}
+
+module.exports = getConversations;
