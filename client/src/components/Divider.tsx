4bbcc2823aabb4515376c022c0ba1e7653b9a393
mohits-git <mohitkhatri2805@gmail.com>
2024-05-12 13:18:50 +0530

    fix: some UI fix and scrollbar css

--- client/src/components/Divider.tsx
+++ client/src/components/Divider.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Divider = () => {
   return (
-    <div className='p-[0.5px] bg-slate-200 my-1'></div>
+    <div className='p-[0.5px] bg-slate-200 mt-1'></div>
   )
 }
 
