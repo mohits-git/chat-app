ebc6b37e9f8a0ad2e97b29e62d474f64fd990f94
mohits-git <mohitkhatri2805@gmail.com>
2024-05-12 13:18:00 +0530

    feat: added center logo when no users selected and added responsiveness

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 import React, { useEffect } from "react";
+import chatLogo from "../assets/chat-logo.svg"
 import { useDispatch, useSelector } from "react-redux";
-import { Outlet, useNavigate } from "react-router-dom";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import { RootState } from "../redux/store";
 import { logout, setToken, setUser } from "../redux/userSlice";
 import toast from "react-hot-toast";
@@ -11,6 +12,7 @@ const Home: React.FC = () => {
   const user = useSelector((state: RootState) => state.user);
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const location = useLocation();
 
   const fetchUserDetails = async () => {
     const URL = `${import.meta.env.VITE_BACKEND_URL}/api/user-details`
@@ -20,7 +22,7 @@ const Home: React.FC = () => {
         withCredentials: true
       });
 
-      if (response.data.logout) {
+      if (response.data.data.logout) {
         dispatch(logout());
         navigate('/login')
         toast("Please login to chat.");
@@ -40,14 +42,30 @@ const Home: React.FC = () => {
     fetchUserDetails();
   }, [])
 
+  const basePath = location.pathname === '/';
+
   return (
     <div className='grid lg:grid-cols-[300px,1fr] h-screen max-h-screen'>
-      <section className="bg-white">
+      <section className={`bg-white ${!basePath && "hidden"} lg:block`}>
         <Sidebar />
       </section>
-      <section>
+      <section className={`${basePath && "hidden"}`}>
         <Outlet />
       </section>
+
+      <div className={`justify-center items-center flex-col gap-2 hidden ${!basePath ? "hidden" : "lg:flex"}`}>
+      <div className="flex justify-center items-center overflow-hidden">
+          <img
+            src={chatLogo}
+            alt="Logo"
+            width={60}
+            height={60}
+            className="text-primary"
+          />
+          <span className="mx-4 text-4xl font-bold text-slate-700">Chat</span>
+        </div>
+        <p className='text-lg mt-2 text-slate-500'>Select user to send message</p>
+      </div>
     </div>
   )
 }
